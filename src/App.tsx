fadf05141ce039cf3e469618d179a8460708bd0b
kamayatu <sarubeji15@gmail.com>
2023-04-27 08:05:12 +0900

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [data, setData] = useState([]);
+  const FetchData = () => {  
+    useEffect(() => {
+      fetch("http://localhost:8000/posts", {method: 'GET'})
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          data.map(post => <li key={post}>{post}</li>)
+        }
+      </ul>
     </div>
   );
 }
