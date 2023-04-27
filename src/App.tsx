47e30674f8758362997435c77fa96fa8114167ad
kamayatu <sarubeji15@gmail.com>
2023-04-27 20:47:58 +0900

    try error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+type Post = {
+  id: number;
+  title: string;
+  status: string;
+  detail?: string;
+}
 
 
 function App() {
-  const [data, setData] = useState([]);
-  const FetchData = () => {  
-    useEffect(() => {
-      fetch("http://localhost:8000/posts", {method: 'GET'})
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      })
-    })
-  }
+  const [data, setData] = useState<Post[] | null>(null);
+  // const FetchData = () => {  
+  //   useEffect(() => {
+  //     fetch("http://localhost:8000/posts", {method: 'GET'})
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setData(data)
+  //     })
+  //   })
+  // }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/posts");
+        const json: React.SetStateAction<Post[] | null> = await res.json();
+      } catch (e) {
+        if(e) {
+          console.log(e);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
+
+
   return (
     <div className="App">
       <ul>
         {
-          data.map(post => <li key={post}>{post}</li>)
+          data?.map((posts) => (
+            <li>{posts.title}</li>
+          ))
         }
       </ul>
     </div>
