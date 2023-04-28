2730eb44364a5683d83c3050169590af2ac6a92e
kamayatu <sarubeji15@gmail.com>
2023-04-28 15:55:57 +0900

    added input area

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,32 @@ type Post = {
 
 function App() {
   const [data, setData] = useState<Post[] | null>(null);
+  const [completeTodos, setCompleteTodos] = useState<Post[] | null>([{
+    id: 200,
+    title: "none",
+    status: "complete"
+  }]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputStatus, setInputStatus] = useState("");
+  const [inputDetail, setInputDetail] = useState("");
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await fetch("/posts");
-  //       const json: React.SetStateAction<Post[] | null> = await res.json();
-  //       setData(json)
-  //     } catch (e) {
-  //       if(e) {
-  //         console.log(e);
-  //       }
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  const onChangeTitle = (e: { target: { value: React.SetStateAction<string>; }; }) => 
+    setInputTitle(e.target.value);
+
+  const onChangeStatus = (e: { target: { value: React.SetStateAction<string>; }; }) => 
+    setInputStatus(e.target.value);
+  
+  const onChangeDetail = (e: { target: { value: React.SetStateAction<string>; }; }) => 
+    setInputDetail(e.target.value);
+
+  const onClickAdd = () => {
+    console.log(inputTitle + inputStatus + inputDetail);
+  }
 
   useEffect(() => {
-    axios.get(baseURL).then(response => {
-      const posts = response.data;
+    axios.get(baseURL)
+    .then(res => {
+      const posts = res.data;
       setData(posts);
     });
   }, []);
@@ -40,15 +47,37 @@ function App() {
   if (!data) return null;
 
   return (
-    <div className="App">
-      <ul>
-        {
-          data?.map((posts) => (
-            <li>{posts.id}</li>
-          ))
-        }
-      </ul>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="タイトル" value={inputTitle} onChange={onChangeTitle}/>
+        <input placeholder="ステータス" value={inputStatus} onChange={onChangeStatus} />
+        <input placeholder="詳細" value={inputDetail} onChange={onChangeDetail}/>
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
+        <ul>
+          {data.map((post) => (
+              <li key={post.id} className="list-row">
+                <p>{post.title}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+          ))}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTodo</p>
+        <ul>
+          {completeTodos?.map((complete) => (
+              <li className="list-row" key={complete.id}>
+              <p>{complete.title}</p>
+              <button>戻す</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
